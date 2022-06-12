f110a53de49c9e76287ae10c9e1dbc10c601be1b
İlker Aksu <ilkeraksu16@users.noreply.github.com>
2022-06-12 14:52:20 +0300

    Entitiy'ler oluşturuldu.
    Base Entitiy, Category, Product ve ProductFeature entitiyleri oluşturuldu.

---
+++ NLayerApp/NLayerApp.Core/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace NLayerApp.Core
+{
+    public class Product:BaseEntity
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public ProductFeature ProductFeature { get; set; }
+
+        /*
+         public int Category_Id { get; set; }
+        [ForeignKey("Category_Id")]
+        public Category Category2 { get; set; }
+
+        Eğer yukarıdaki gibi bir foreigkey id tanımlaması yapsaydık, Category2 nesnesi üzerinden Foreigkey attiribute ile belirtmemiz gerekecektik, Yukarıdaki gibi best practics tanımlamayla gerek kalmadı.
+         
+         */
+    }
+}
