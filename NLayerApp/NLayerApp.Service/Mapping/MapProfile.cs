3139280f10f9adbfea5e1aa25bdd7cba367df5c3
İlker Aksu <ilkeraksu16@users.noreply.github.com>
2022-06-18 21:41:30 +0300

    Response classları oluşturuldu.

---
+++ NLayerApp/NLayerApp.Service/Mapping/MapProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using NLayerApp.Core.DTOs;
+using NLayerApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.Service.Mapping
+{
+    public class MapProfile:Profile
+    {
+        public MapProfile()
+        {
+            // reverseMap ile iki sınıfında birbirine dönüştürülebilmesini sağladık.
+            // ReverseMap olmasaydı yalnızca Product->ProductDto'ya dönüştürülürdü.
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<ProductFeature, ProductFeatureDto>().ReverseMap();
+            CreateMap<ProductUpdateDto, Product>();
+        }
+    }
+}
